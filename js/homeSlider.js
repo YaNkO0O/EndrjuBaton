ea04909d3724b90131412716e342c3b17771c1b4
Anavriinn <gabriela.roszak2807@wp.pl>
2022-11-15 14:15:01 +0100

    "#6 - banery"

--- js/homeSlider.js
+++ js/homeSlider.js
@@ -1,14 +1,15 @@
-const next = document.querySelector(".homeSlider__next");
-const prev = document.querySelector(".homeSlider__prev");
-
 let slideIndex = [1, 1];
-let slideName = ["homeSlider__photo"];
+let slideId = ["homeSlider__photo"];
 showSlides(1, 0);
 showSlides(1, 1);
 
+function plusSlides(n, no) {
+	showSlides((slideIndex[no] += n), no);
+}
+
 function showSlides(n, no) {
 	let i;
-	let x = document.getElementsByClassName(slideName[no]);
+	let x = document.getElementsByClassName(slideId[no]);
 	if (n > x.length) {
 		slideIndex[no] = 1;
 	}
@@ -18,11 +19,5 @@ function showSlides(n, no) {
 	for (i = 0; i < x.length; i++) {
 		x[i].style.display = "none";
 	}
-	console.log(x);
 	x[slideIndex[no] - 1].style.display = "block";
 }
-function plusSlides(n, no) {
-	showSlides((slideIndex[no] += n), no);
-}
-// next.addEventListener("click", plusSlides(1, 0));
-// prev.addEventListener("click", plusSlides(-1, 0));
